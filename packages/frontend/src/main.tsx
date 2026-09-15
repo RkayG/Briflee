31f87f89361289d37bd64c0957ef3724e8de7ef5
RkayG <rufus.kenny09@gmail.com>
2026-09-15 09:12:53 +0100

    feat: add CreateProject route and update brand color variables in theme

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Dashboard } from "@/pages/dashboard";
 import { Projects } from "@/pages/projects";
+import { CreateProject } from "@/pages/create-project";
 import { Clients } from "@/pages/clients";
 import { ClientPortal } from "@/pages/client-portal";
 import { NotFound } from "@/pages/not-found";
@@ -18,6 +19,7 @@ createRoot(document.getElementById("root")!).render(
                     <Routes>
                         <Route path="/" element={<Dashboard />} />
                         <Route path="/projects" element={<Projects />} />
+                        <Route path="/projects/new" element={<CreateProject />} />
                         <Route path="/clients" element={<Clients />} />
                         <Route path="/client-portal" element={<ClientPortal />} />
                         <Route path="*" element={<NotFound />} />
